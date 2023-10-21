1b7eec9e8a0d317743d64f686d7a20d50e334600
bahare <pondblue07@gmail.com>
2023-10-21 19:01:22 +0330

    eraser and brush tools implemented

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ const canvas = document.querySelector('canvas')
 const ctx = canvas.getContext('2d')
 const brushWidth = document.querySelector('#brush-width')
 const brushColor = document.querySelector('#color-picker')
-
-
+const eraser = document.querySelector('.eraser')
+const brush = document.querySelector('.brush')
 
 let isDrawing = false
 let currentWidth = 5
@@ -17,6 +17,18 @@ window.addEventListener('load' , () => {
 canvas.addEventListener('mousedown' , startDraw)
 canvas.addEventListener('mousemove' , drawing)
 canvas.addEventListener('mouseup' , endDraw)
+
+eraser.addEventListener('click' , () => {
+    eraser.classList.add('active')
+    brush.classList.remove('active')
+    currentColor = 'white'
+})
+
+brush.addEventListener('click' , () => {
+    brush.classList.add('active')
+    eraser.classList.remove('active')
+    currentColor = brushColor.value
+})
 
 brushWidth.addEventListener('change' , () => {
     currentWidth = brushWidth.value
